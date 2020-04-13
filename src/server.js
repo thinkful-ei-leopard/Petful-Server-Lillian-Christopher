f857d4e2ad869cc7e1533280fbd4cfa7b3875986
Christopher Floyd <cfloyd252@gmail.com>
2020-04-13 00:54:26 -0700

    fixing deployment

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const app = require('./app');
-const { PORT } = process.env.PORT || 8000;
+const  PORT  = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
